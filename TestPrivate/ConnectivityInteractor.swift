f9f8a6e7d149d295826e9e3999c9e01ad4a8a7c3
Dan Ursu <dan.ursu@yopeso.com>
2016-05-20 17:47:40 +0200

    Initial commit

---
+++ TestPrivate/ConnectivityInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  ConnectivityInteractor.swift
+//  TestPrivate
+//
+//  Created by Dan Ursu on 20/05/16.
+//  Copyright © 2016 rooomy. All rights reserved.
+//
+
+import Foundation
+
+class ConnectivityChecker: NSObject {
+    private var reachability: Bool?
+}
