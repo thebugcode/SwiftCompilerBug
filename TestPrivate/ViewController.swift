90d002c2f8a94bbdf23d04a32cde6c9799ad5e74
Dan Ursu <dan.ursu@yopeso.com>
2016-05-20 14:51:52 +0200

    Initial Commit

---
+++ TestPrivate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestPrivate
+//
+//  Created by Dan Ursu on 20/05/16.
+//  Copyright © 2016 rooomy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
